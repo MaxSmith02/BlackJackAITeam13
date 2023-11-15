72b23879dfe2b9ca57d6acff7ec145b167e43b57
Liswerd <dragonweirdo4714@gmail.com>
2023-11-15 14:10:22 -0500

    was me :P

--- src/Deck.java
+++ src/Deck.java
@@ -1,2 +1,15 @@
+import java.util.LinkedList;
+
 public class Deck {
+    private LinkedList<Card> theDeck;
+
+    public Deck()
+    {
+        Card fill;
+        for(int i = 1; i < 14; i++)
+        {
+            fill = new Card(1, "Heart");
+
+        }
+    }
 }
