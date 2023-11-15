c8a44c4260fa44b2dca5e52a6054e58265ba3e99
Liswerd <dragonweirdo4714@gmail.com>
2023-11-15 14:16:26 -0500

    Added assumed all blank classes

--- src/Deck.java
+++ src/Deck.java
@@ -8,8 +8,28 @@ public class Deck {
         Card fill;
         for(int i = 1; i < 14; i++)
         {
-            fill = new Card(1, "Heart");
-
+            fill = new Card(1, "Hearts");
+            theDeck.add(fill);
         }
+        for(int i = 1; i < 14; i++)
+        {
+            fill = new Card(1, "Spades");
+            theDeck.add(fill);
+        }
+        for(int i = 1; i < 14; i++)
+        {
+            fill = new Card(1, "Diamonds");
+            theDeck.add(fill);
+        }
+        for(int i = 1; i < 14; i++)
+        {
+            fill = new Card(1, "Clubs");
+            theDeck.add(fill);
+        }
+    }
+
+    public void shuffleDeck()
+    {
+
     }
 }
