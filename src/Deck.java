ad1b67943ff96bc7e08c8e72d493760c8bdf1275
Liswerd <dragonwerdo4714@gmail.com>
2023-12-02 14:49:11 -0500

    Implimenting a shuffle function and a draw function. To make sure no information is lost on drawn cards, added new private variable in Deck, revealedCards

--- src/Deck.java
+++ src/Deck.java
@@ -1,41 +1,53 @@
 import java.util.LinkedList;
-
+import java.util.Random;
 public class Deck {
     private LinkedList<Card> theDeck;
 
     public Deck()
     {
+        this.theDeck = new LinkedList<Card>();
         Card fill;
         for(int i = 1; i < 14; i++)
         {
             fill = new Card(i, "Hearts");
-            theDeck.add(fill);
+            this.theDeck.add(fill);
         }
         for(int i = 1; i < 14; i++)
         {
             fill = new Card(i, "Spades");
-            theDeck.add(fill);
+            this.theDeck.add(fill);
         }
         for(int i = 1; i < 14; i++)
         {
             fill = new Card(i, "Diamonds");
-            theDeck.add(fill);
+            this.theDeck.add(fill);
         }
         for(int i = 1; i < 14; i++)
         {
             fill = new Card(i, "Clubs");
-            theDeck.add(fill);
+            this.theDeck.add(fill);
         }
     }
 
     //Shuffles the deck
     public void shuffleDeck()
     {
-        LinkedList<Card> shuffle = null;
-
-        for(int i = 0; i < theDeck.size(); i++)
+        LinkedList<Card> shuffle = new LinkedList<Card>();
+        Random n = new Random();
+        int randVal = n.nextInt(theDeck.size());
+        for(int i = 0; i < 52; i++)
         {
-
+            randVal = n.nextInt(theDeck.size());
+            shuffle.add(theDeck.get(randVal));
+            theDeck.remove(randVal);
+            //theDeck.remove(n);
         }
+        theDeck = shuffle;
     }
+
+    public Card drawCard()
+    {
+        return theDeck.removeFirst();
+    }
+
 }
